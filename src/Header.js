2b04078e6d33acc820afc8f37485216213bf1a33
unknown <avanidhalgara01@gmail.com>
2020-10-29 22:00:32 -0500

    header created

---
+++ src/Header.js
@@ -0,0 +1,26 @@
+import React from "react";
+import "./Header.css";
+import PersonIcon from "@material-ui/icons/Person";
+import IconButton from "@material-ui/core/IconButton";
+import ForumIcon from '@material-ui/icons/Forum';
+
+function Header() {
+  return (
+    <div className = "header">
+      
+      <IconButton>
+        <PersonIcon fontSize="large" className="header-icon" />
+      </IconButton>
+      <img className = "header_logo"
+      src = "https://cdn2.iconfinder.com/data/icons/social-media-icons-23/800/tinder-512.png"
+      alt = "tinder"
+      />
+      <IconButton>
+         <ForumIcon fontSize="large" className="header-icon"/>
+      </IconButton>
+      
+    </div>
+  );
+}
+
+export default Header;
